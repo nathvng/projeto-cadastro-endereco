2f9a051115c788bcb00332bc47231762042c3183
nathvng <nathaliagabrielevenegasiqueira@gmail.com>
2025-04-29 10:16:49 -0300

    Criação do Backend JAVA

---
+++ src/main/java/com/senai/cadastroendereco/entities/Pessoa.java
@@ -0,0 +1,92 @@
+package com.senai.cadastroendereco.entities;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_pessoa")
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPessoa;
+    @Column(name ="nome")
+    private String nome;
+    @Column(name ="email")
+    private String email;
+    @Column(name ="telefone")
+    private String telefone;
+    @Column(name ="senha")
+    private String senha;
+    
+    @OneToMany(mappedBy = "pessoa")
+    private List<Endereco> enderecos;
+    
+    public Pessoa() {
+        
+    }
+    
+    public Pessoa(Long idPessoa, String nome, String email, String telefone, String senha) {
+        this.idPessoa = idPessoa;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+    }
+
+    public Long getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(Long idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+    
+    
+}
---
+++ src/main/java/com/senai/cadastroendereco/repository/PessoaRepository.java
@@ -0,0 +1,11 @@
+package com.senai.cadastroendereco.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.senai.cadastroendereco.entities.Pessoa;
+
+public interface PessoaRepository extends JpaRepository <Pessoa, Long>{
+	
+	//Busca a pessoa pelo email
+	Pessoa findByEmail(String email);
+}
